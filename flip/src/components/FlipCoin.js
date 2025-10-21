211d5a9db45db5bb36e979f6ec313918b3608d62
gaurav <gauravgs.1290@gmail.com>
2025-10-21 22:26:51 +0530

    I have added a loader while flipping the coin

--- flip/src/components/FlipCoin.js
+++ flip/src/components/FlipCoin.js
@@ -1,78 +1,80 @@
-// FlipCoin.js
-
-import React, { Component } from 'react'
-import Coin from './Coin'
+import React, { Component } from 'react';
+import Coin from './Coin';
 
 class FlipCoin extends Component {
-    static defaultProps = {
-        coins: [
-            // Sides of the coin
-            {
-                side: 'head', 
-                imgSrc:
-'https://media.geeksforgeeks.org/wp-content/uploads/20200916123059/SHalfDollarObverse2016head-300x300.jpg'
-            },
-            {
-                side: 'tail', 
-                imgSrc:
-'https://media.geeksforgeeks.org/wp-content/uploads/20200916123125/tails-200x200.jpg'
-            }
-        ]
-    }
+  static defaultProps = {
+    coins: [
+      {
+        side: 'head',
+        imgSrc:
+          'https://media.geeksforgeeks.org/wp-content/uploads/20200916123059/SHalfDollarObverse2016head-300x300.jpg'
+      },
+      {
+        side: 'tail',
+        imgSrc:
+          'https://media.geeksforgeeks.org/wp-content/uploads/20200916123125/tails-200x200.jpg'
+      }
+    ]
+  };
 
-    constructor(props) {
-        super(props)
-        // Responsible to render current updated coin face
-        this.state = {
-            // Track total number of flips
-            currFace: null,
-            totalFlips: 0,
-            heads: 0
-        }
-        // Binding context of this keyword
-        this.handleClick = this.handleClick.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      currFace: null,
+      totalFlips: 0,
+      heads: 0,
+      loading: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    // Function takes array of different faces of a coin
-    //  and return a random chosen single face
-    choice(arr) {
-        const randomIdx = Math.floor(Math.random() * arr.length)
-        return arr[randomIdx]
-    }
+  choice(arr) {
+    const randomIdx = Math.floor(Math.random() * arr.length);
+    return arr[randomIdx];
+  }
 
-    // Function responsible to update the states
-    // according to users interactions
-    flipCoin() {
-        const newFace = this.choice(this.props.coins)
-        this.setState(curState => {
-            const heads = curState.heads +
-                (newFace.side === 'head' ? 1 : 0)
-            return {
-                currFace: newFace,
-                totalFlips: curState.totalFlips + 1,
-                heads: heads
-            }
-        })
-    }
-    handleClick() {   
-        this.flipCoin()
-    }
-    render() {
-        const { currFace, totalFlips, heads } = this.state
-        return (
-            <div>
-                <h2>Let's flip a coin</h2>
-                {/* If current face exist then show current face */}
-                {currFace && <Coin info={currFace} />}
-                {/* Button to flip the coin  */}
-                <button onClick={this.handleClick}>Flip Me!</button>
-                <p>
-                    Out of {totalFlips} flips, there have been {heads} heads
-                    and {totalFlips - heads} tails
-                </p>
-            </div>
-        )
-    }
+  flipCoin() {
+    
+    this.setState({ loading: true });
+    setTimeout(() => {
+      const newFace = this.choice(this.props.coins);
+      this.setState(curState => {
+        const heads = curState.heads + (newFace.side === 'head' ? 1 : 0);
+        return {
+          currFace: newFace,
+          totalFlips: curState.totalFlips + 1,
+          heads: heads,
+          loading: false 
+        };
+      });
+    }, 1000);
+  }
+
+  handleClick() {
+    this.flipCoin();
+  }
+
+  render() {
+    const { currFace, totalFlips, heads, loading } = this.state;
+    return (
+      <div>
+        <h2>Let's flip a coin</h2>
+
+        {loading ? (
+          <div className='loader'></div>
+        ) : (
+          currFace && <Coin info={currFace} />
+        )}
+
+        <button onClick={this.handleClick}>Flip Me!</button>
+
+        <p>
+          Out of {totalFlips} flips, there have been {heads} heads and{' '}
+          {totalFlips - heads} tails.
+        </p>
+      </div>
+    );
+  }
 }
 
-export default FlipCoin
+export default FlipCoin;
